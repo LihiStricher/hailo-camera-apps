1371922c987c5184d3167e8ec00591dcd1f88044
HailoVPU-Automation <hailovpu-automation@hailo.ai>
2025-07-14 20:57:55 +0300

    v1.7.9_test_pluto-dv-attempt_19

--- apps/h15/native/reference_camera_api/reference_camera_infra.cpp
+++ apps/h15/native/reference_camera_api/reference_camera_infra.cpp
@@ -15,5 +15,10 @@
 #include "pipeline_infra/udp_stage.hpp"
 #include "pipeline_infra/file_stage.hpp"
 #include "pipeline_infra/reference_camera_logger.hpp"
+#include "pipeline_infra/valve_stage.hpp"
+#include "pipeline_infra/freeze_stage.hpp"
+#include "pipeline_infra/scale_bbox_stage.hpp"
+#include "pipeline_infra/tracker_traffic_ctrl_stage.hpp"
+#include "pipeline_infra/zmq_comm_stage.hpp"
 
 // Header consolidation file
